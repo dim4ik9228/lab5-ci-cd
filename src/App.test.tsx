17484b3daabdaf69b497907f5a11dcd90cce32c1
Dmytro Stepaniuk <dimaras0909@gmail.com>
2025-04-14 00:38:38 +0300

    changed folder structure that was wrong for some reason

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { describe, it, expect } from 'vitest';
-import App from '../App';
+import App from './App';
 
 describe('App', () => {
   it('renders headline', () => {
